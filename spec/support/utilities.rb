442a064217280d97d6a01ba6dbeb0f54e5a468ab
Alex Hsi <alexhhsi@gmail.com>
2013-06-03 17:23:13 -0700

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,11 +1,5 @@
 include ApplicationHelper
 
-def valid_signin(user)
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: user.password
-  click_button "Sign in"
-end
-
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     expect(page).to have_selector('div.alert.alert-error', text: message)
@@ -19,4 +13,13 @@ def full_title(page_title)
   else
     "#{base_title} | #{page_title}"
   end
+end
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
 end
